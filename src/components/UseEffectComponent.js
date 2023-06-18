efa26d55fb8730dd474c82de83ce66ae2b81af76
Hemendra Singh Shaktawat <hemendra387@gmail.com>
2023-06-19 00:47:11 +0530

    useEffect with condition

--- src/components/UseEffectComponent.js
+++ src/components/UseEffectComponent.js
@@ -3,18 +3,27 @@ import { useState, useEffect } from "react";
 export function UseEffectComponent() {
 
     const [count, setCount] = useState(0);
+    const [num, setNumber] = useState(0);
 
     useEffect(() => {
-        console.log('this is use effect hook');
-    })
+        console.log('this is use effect hook for count state only!!!');
+    }, [count]) // pass props.propsName as parameter if need to call hook on specific props
+
+    useEffect(() => {
+        console.log('this is use effect hook for num state only!!!');
+    }, [num])
 
     return (
         <>
             <h2>Use Effect Component</h2>
             <br />
-            <h2>{count}</h2>
+            <h2> Count : {count}</h2>
             <br />
             <button onClick={() => setCount(count + 1)}>Increase Count</button>
+            <br />
+            <h2> Number : {num}</h2>
+            <br />
+            <button onClick={() => setNumber(num + 10)}>Increase Number</button>
         </>
     )
 }
