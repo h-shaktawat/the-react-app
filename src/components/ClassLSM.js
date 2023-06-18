9e7f1baeedb6c9a58d14d91c3aa6fe7c92ad046c
Hemendra Singh Shaktawat <hemendra387@gmail.com>
2023-06-19 00:03:53 +0530

    should Component Update

--- src/components/ClassLSM.js
+++ src/components/ClassLSM.js
@@ -5,27 +5,42 @@ export default class ClassLCM extends Component {
     constructor() {
         // Run each type
         super();
-        console.warn("this is constructor!!!");
+
+        this.state = {
+            count: 0
+        }
+
+        console.log("this is constructor!!!");
     }
 
     componentDidMount() {
         // Run only when component call first time
-        console.warn("this is componentDidMount!!!");
+        console.log("this is componentDidMount!!!");
     }
 
     componentDidUpdate(preProps, preState, snapshot) {
         // Run when state and props get updated
-        console.warn("this is componentDidUpdate!!!");
+        console.log("this is componentDidUpdate!!!");
+    }
+
+    shouldComponentUpdate() {
+        console.log("this is shouldComponentUpdate!!!");
+        return true; // Set false if no need to re-render component
     }
 
     render() {
 
         // Run each type when component render
-        console.warn("this is render!!!");
+        console.log("this is render!!!");
 
         return (
             <>
-                <h3>Hello from Class Life Cycle Methods</h3>
+                <h3>Hello from Class Component Life Cycle Methods</h3>
+                <br />
+                <h1>{this.state.count}</h1>
+                <br />
+                <br />
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>Increase Count</button>
             </>
         )
     }
