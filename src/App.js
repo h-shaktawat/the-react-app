7d892a2a49bb8aef2ad829d6bad55b6cac7096af
Hemendra Singh Shaktawat <hemendra387@gmail.com>
2023-06-19 01:02:26 +0530

    Style in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import { UseEffectComponent } from './components/UseEffectComponent';
+import './assets/custom.css'
+import style from './assets/style.module.css'
 
 function App() {
   return (
     <div className="App">
-      <UseEffectComponent />
+
+      <h1 className='external'>
+        External Style Heading
+      </h1>
+
+      <h1 style={{ color: "red", backgroundColor: "blue" }}>
+        Inline Style Heading
+      </h1>
+
+      <h1 className={style.module}>
+        Module Style Heading
+      </h1>
+      
     </div>
   );
 }
