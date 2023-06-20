3977a853e56d69a4e79ab0dc6c76ad41f53db12c
Hemendra Singh Shaktawat <hemendra387@gmail.com>
2023-06-21 00:59:52 +0530

    Functional Component

---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,14 @@
+function FunctionalComponent(props) {
+
+    return (
+        <>
+        
+            {/* 
+                This component is for all type of functionalities in react functional component!!!
+            */}
+
+        </>
+    );
+}
+
+export default FunctionalComponent;
